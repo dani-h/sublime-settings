0a1a7c94012029d3d3ba501e18fc9dcbb061b8b7
Dani Hodovic <danihgit@gmail.com>
2014-11-20 14:33:19 +0100

    Test

--- TabsToSpacesOnSave/tabs_to_spaces_on_save.py
+++
@@ -1,7 +0,0 @@
-import sublime, sublime_plugin
-
-class ExpandTabsOnSave(sublime_plugin.EventListener):
-  # Run ST's 'expand_tabs' command when saving a file
-  def on_pre_save(self, view):
-    if view.settings().get('expand_tabs_on_save') == 1:
-      view.window().run_command('expand_tabs')
